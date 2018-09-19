8440f7291e1f3b7c9e64aa31396b8424b6318256
Davide <davidebelli95@gmail.com>
2018-09-19 16:08:56 +0200

    Fixed LSTM

--- dl4nlt/models/lstm/lstm.py
+++ dl4nlt/models/lstm/lstm.py
@@ -3,8 +3,10 @@ import torch.nn as nn
 from torch.nn import init
 import numpy as np
 
+
 class CustomLSTM(nn.Module):
-    def __init__(self, vocab_len, emb_size, n_hidden_units, n_hidden_layers=1, n_output=1, dropout=0.2, rnn_type='LSTM', from_file=None):
+    def __init__(self, vocab_len, emb_size, n_hidden_units, n_hidden_layers=1, n_output=1, dropout=0.2, rnn_type='LSTM',
+                 from_file=None):
         super(CustomLSTM, self).__init__()
         
         torch.manual_seed(42)
@@ -20,26 +22,27 @@ class CustomLSTM(nn.Module):
         self.encoder = nn.Embedding(vocab_len, emb_size, padding_idx=0)
         if rnn_type in ['LSTM', 'GRU', 'BLTSM']:
             is_bidirectional = True if rnn_type == 'BLSTM' else False
-            self.rnn = getattr(nn, rnn_type)(emb_size, n_hidden_units, n_hidden_layers, dropout=dropout, bidirectional=is_bidirectional)
+            self.rnn = getattr(nn, rnn_type)(emb_size, n_hidden_units, n_hidden_layers,
+                                             dropout=dropout,
+                                             bidirectional=is_bidirectional)  # Dropout added only for n_layers > 1
         else:
             raise ValueError("An invalid option for `--model` was supplied, options are ['LSTM', 'GRU', 'BLSTM']")
         
         self.decoder = nn.Linear(n_hidden_units, self.linear_size)
         self.decoder2 = nn.Linear(self.linear_size, n_output)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Sigmoid()
         
         if from_file is not None:
             self.init_emb_from_file(from_file)
-    
+        
         # self.init_weights()
     
-
     def init_weights(self):
         initrange = 0.1
         self.encoder.weight.data.uniform_(-initrange, initrange)
         self.decoder.bias.data.fill_(0)
         self.decoder.weight.data.uniform_(-initrange, initrange)
-
+    
     def forward(self, input, hidden):
         emb = self.drop(self.encoder(input))
         output, hidden = self.rnn(emb, hidden)
@@ -51,18 +54,25 @@ class CustomLSTM(nn.Module):
         output = self.softmax(output)
         result = output.view(-1)
         return result, hidden
-
+    
     def init_hidden(self, bsz):
         weight = next(self.parameters()).data
-        return weight.new(self.n_hidden_layers, bsz, self.n_hidden_units).zero_()
-
+        
+        if self.rnn_type == 'LSTM':
+            return (weight.new(self.n_hidden_layers, bsz, self.n_hidden_units).zero_(),
+                    weight.new(self.n_hidden_layers, bsz, self.n_hidden_units).zero_())
+        else:
+            return weight.new(self.n_hidden_layers, bsz, self.n_hidden_units).zero_()
+    
     def init_emb_from_file(self, path):
         emb_mat = np.genfromtxt(path)
         self.encoder.weight.data.copy_(torch.from_numpy(emb_mat))
-        
-        
-rnn = CustomLSTM(5, 10, 20, 1)
-input = torch.randint(0, 5, (4, 2))
-h0 = torch.randn(2, 3, 20)
-c0 = torch.randn(2, 3, 20)
-output, (hn, cn) = rnn(input, (h0, c0))
+
+
+if __name__ == "__main__":
+    rnn = CustomLSTM(5, 10, 20, 1)
+    input = torch.randint(0, 5, (4, 2)).type(torch.LongTensor)
+    h0 = torch.randn(1, 2, 20)
+    c0 = torch.randn(1, 2, 20)
+    output, hn = rnn(input, (h0, c0))
+    print(output)
