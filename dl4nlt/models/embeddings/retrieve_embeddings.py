245c26266aaa6e6d2ccd3621d7b86be34f42e4d3
alexgeenen <geenen124@gmail.com>
2018-09-27 18:52:07 +0200

    Add Pre-Trained Glove Embedding Layer

--- dl4nlt/models/embeddings/retrieve_embeddings.py
+++ dl4nlt/models/embeddings/retrieve_embeddings.py
@@ -1,14 +1,18 @@
 import wget
 import zipfile
 import os
+import pickle
+import bcolz
+import numpy as np
 
 elmo_options_url = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 elmo_weights_url = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
 glove_weights_url = "http://nlp.stanford.edu/data/wordvecs/glove.6B.zip"
 
-elmo_options_file = "../../data/elmo_options.json"
-elmo_weights_file = "../../data/elmo_weights.hdf5"
-glove_weights_file = "../../data/glove_weights.dat"
+data_dir = os.path.join(os.path.dirname(__file__), "../../data/")
+elmo_options_file = os.path.join(data_dir, "elmo_options.json")
+elmo_weights_file = os.path.join(data_dir, "elmo_weights.hdf5")
+glove_weights_file = os.path.join(data_dir, "glove_weights.dat")
 
 
 def download_elmo_weights():
@@ -29,23 +33,57 @@ def download_elmo_weights():
 
 
 def download_glove_weights():
-    glove_zip_file = "../../data/glove_weights.zip"
-    if os.path.exists(glove_zip_file):
-        return
+    glove_zip_file = os.path.join(data_dir, "glove_weights.zip")
+    if not os.path.exists(glove_zip_file):
+        print("Downloading Glove Weights...")
+        wget.download(
+            "http://nlp.stanford.edu/data/wordvecs/glove.6B.zip",
+            out=glove_zip_file,
+            bar=wget.bar_thermometer
+        )
+        print("Finished downloading Glove Weights...")
 
-    print("Downloading Glove Weights...")
-    wget.download(
-        "http://nlp.stanford.edu/data/wordvecs/glove.6B.zip",
-        out=glove_zip_file,
-        bar=wget.bar_thermometer
-    )
-
+    print("Extracting Glove Weights...")
     with zipfile.ZipFile(glove_zip_file, "r") as zip_ref:
-        zip_ref.extractall()
+        zip_ref.extractall(path=data_dir)
+    print("Finished extracting Glove Weights")
 
-    print("Finished downloading Glove Weights...")
+
+def preprocess_glove_weights():
+    """
+    Preprocess the Glove weights for use in Pytorch embedding
+
+    https://medium.com/@martinpella/how-to-use-pre-trained-word-embeddings-in-pytorch-71ca59249f76
+
+    """
+    print("Preparing Glove Weights")
+    tokens = []
+    index = 0
+    t2ind = {}
+    embeddings = bcolz.carray(np.zeros(1), rootdir=os.path.join(data_dir, "glove_data.dat"), mode="w")
+
+    with open(os.path.join(data_dir, "glove.6B.50d.txt"), "rb") as weights_file:
+        for line in weights_file:
+            line_vals = line.decode().split()
+            token = line_vals[0]
+            tokens.append(token)
+
+            t2ind[token] = index
+            index += 1
+
+            embedding = np.array(line_vals[1:]).astype(np.float)
+            embeddings.append(embedding)
+
+    embeddings = bcolz.carray(embeddings[1:].reshape((-1, 50)),
+                              rootdir=os.path.join(data_dir, "glove_data.dat"),
+                              mode='w')
+    embeddings.flush()
+    pickle.dump(tokens, open(os.path.join(data_dir, "glove_tokens.pkl"), "wb"))
+    pickle.dump(t2ind, open(os.path.join(data_dir, "glove_t2ind.pkl"), "wb"))
+    print("Finished Preparing Glove Weights")
 
 
 if __name__ == "__main__":
     download_elmo_weights()
     download_glove_weights()
+    preprocess_glove_weights()
