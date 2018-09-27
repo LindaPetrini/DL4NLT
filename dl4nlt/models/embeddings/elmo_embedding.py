aa27eb73bcf2b7fc0d2305d73e45755c129ec299
alexgeenen <geenen124@gmail.com>
2018-09-27 18:57:28 +0200

    Fix Imports & Download Elmo Weights If Not Present
    The elmo weights will automatically be downloaded if
    a call is made to create an Elmo embedding layer and
    they aren't already present

--- dl4nlt/models/embeddings/elmo_embedding.py
+++ dl4nlt/models/embeddings/elmo_embedding.py
@@ -1,11 +1,16 @@
+import os
 from allennlp.modules.elmo import Elmo, batch_to_ids
+from dl4nlt.models.embeddings.retrieve_embeddings import download_elmo_weights
 
-DEFAULT_ELMO_LOCATION = "../../data"
+data_dir = os.path.join(os.path.dirname(__file__), "../../data")
 
 
-def create_elmo_embedding(elmo_weights_folder=DEFAULT_ELMO_LOCATION):
-    elmo_options = f"{elmo_weights_folder}/elmo_options.json"
-    elmo_weights = f"{elmo_weights_folder}/elmo_weights.hdf5"
+def create_elmo_embedding(elmo_weights_folder=data_dir):
+    elmo_options = os.path.join(elmo_weights_folder, "elmo_options.json")
+    elmo_weights = os.path.join(elmo_weights_folder, "elmo_weights.hdf5")
+
+    if not os.path.exists(elmo_options):
+        download_elmo_weights()
 
     elmo = Elmo(elmo_options, elmo_weights, 2, dropout=0)
     return elmo
